2b9ec8fea40abafbf3166e082d541ad00513d1ac
John Steinmetz <john.steinmetz@bazaarvoice.com>
2016-07-29 14:50:57 -0500

    Updated db info for heroku

--- data/database.php
+++ data/database.php
@@ -1,6 +1,6 @@
 <?php
-    $server = "127.0.0.1";
-    $username = "root";
-    $password = "root";
-    $database = "bvpixel";
+    $server = "nj5rh9gto1v5n05t.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+    $username = "c2nsu8zp0uywwqpd";
+    $password = "sn8snb2d2fx6q7o1";
+    $database = "dz8o702dnm4e7snp";
 ?>
